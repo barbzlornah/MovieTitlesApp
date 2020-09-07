70f1091557ceeef41f1830e949ebbb4c6e6fb014
Barbara Masinde <barbzlornah@gmail.com>
2020-09-07 18:49:47 +0300

    Add project files.

---
+++ McvMovie/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace McvMovie.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
